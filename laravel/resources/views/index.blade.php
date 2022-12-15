e5c9b9032b8a776411f722f42642de84009f4173
rintarouchida <ww.stamford@gmail.com>
2022-12-15 22:10:09 +0900

    feat-view

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,33 @@
+@extends('app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="text-left">
+                <h2 style="font-size:1rem;">文房具マスター</h2>
+            </div>
+            <div class="text-right">
+            <a class="btn btn-success" href="#">新規登録</a>
+            </div>
+        </div>
+    </div>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>name</th>
+            <th>kakaku</th>
+            <th>bunrui</th>
+        </tr>
+        @foreach ($bunbougus as $bunbougu)
+        <tr>
+            <td style="text-align:right">{{ $bunbougu->id }}</td>
+            <td>{{ $bunbougu->name }}</td>
+            <td style="text-align:right">{{ $bunbougu->kakaku }}円</td>
+            <td style="text-align:right">{{ $bunbougu->bunrui }}</td>
+        </tr>
+        @endforeach
+    </table>
+
+    {!! $bunbougus->links() !!}
+@endsection
