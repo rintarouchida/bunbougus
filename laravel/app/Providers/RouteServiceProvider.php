1f54b05e52a56a0993a65892d13df40b5dbcdc42
rintarouchida <ww.stamford@gmail.com>
2022-12-31 21:33:49 +0900

    setting

--- laravel/app/Providers/RouteServiceProvider.php
+++ laravel/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/bunbougu';
 
     /**
      * The controller namespace for the application.
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,14 +19,14 @@ Route::get('/', function () {
 Route::get('/bunbougu', 'App\Http\Controllers\BunbouguController@index')
 ->name('bunbougu.index');
 
-Route::get('bunbougu/create', 'App\Http\Controllers\BunbouguController@create')->name('bunbougu.create');
-Route::post('bunbougu/store', 'App\Http\Controllers\BunbouguController@store')->name('bunbougu.store');
-Route::get('/bunbougus/edit/{bunbougu}', 'App\Http\Controllers\BunbouguController@edit')->name('bunbougu.edit');
-Route::put('/bunbougus/edit/{bunbougu}','App\Http\Controllers\BunbouguController@update')->name('bunbougu.update');
+Route::get('bunbougu/create', 'App\Http\Controllers\BunbouguController@create')->name('bunbougu.create')->middleware('auth');
+Route::post('bunbougu/store', 'App\Http\Controllers\BunbouguController@store')->name('bunbougu.store')->middleware('auth');
+Route::get('/bunbougus/edit/{bunbougu}', 'App\Http\Controllers\BunbouguController@edit')->name('bunbougu.edit')->middleware('auth');
+Route::put('/bunbougus/edit/{bunbougu}','App\Http\Controllers\BunbouguController@update')->name('bunbougu.update')->middleware('auth');
 
-Route::get('/bunbougus/show/{bunbougu}', 'App\Http\Controllers\BunbouguController@show')->name('bunbougu.show');
+Route::get('/bunbougus/show/{bunbougu}', 'App\Http\Controllers\BunbouguController@show')->name('bunbougu.show')->middleware('auth');
 
-Route::delete('/bunbougus/{bunbougu}', 'App\Http\Controllers\BunbouguController@destroy')->name('bunbougu.destroy');
+Route::delete('/bunbougus/{bunbougu}', 'App\Http\Controllers\BunbouguController@destroy')->name('bunbougu.destroy')->middleware('auth');
 
 Auth::routes();
 
